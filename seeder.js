415a0dde681ea9688ff66df3e189d6dc61609a2d
SATATA <sadiqul842@gmail.com>
2020-10-10 23:20:21 +0600

    add course model

--- seeder.js
+++ seeder.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config({path: './config/config.env'});
 // Load models
 const Bootcamp = require('../simple-node-project/models/Bootcamp');
-// const Course = require('./models/Course');
+const Course = require('./models/Course');
 // const User = require('./models/User');
 // const Review = require('./models/Review');
 // Connect to DB
@@ -20,21 +20,21 @@ const bootcamps = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 //
-// const courses = JSON.parse(
-//   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
-// );
+const courses = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 //
 // const users = JSON.parse(
 //   fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 // );
-const reviews = JSON.parse(
-    fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
-);
+// const reviews = JSON.parse(
+//     fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
+// );
 // Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
-        //  await Course.create(courses);
+        await Course.create(courses);
         // await User.create(users);
         // await Review.create(reviews);
         console.log('Data Imported...');
